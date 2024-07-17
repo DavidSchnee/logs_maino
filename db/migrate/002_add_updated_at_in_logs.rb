e5121df8c8e022b7641c80ac374bda788d53c0a1
David Schnee <david.schnee@maino.com.br>
2024-07-17 10:56:16 -0300

    fix migrate

--- db/migrate/002_add_updated_at_in_logs.rb
+++ db/migrate/002_add_updated_at_in_logs.rb
@@ -3,7 +3,7 @@
 Sequel.migration do
   change do
     alter_table :logs do
-      remove_column :updated_at, DateTime, index: true  # Replace with your column name and type
+      add_column :updated_at, DateTime, index: true
     end
   end
 end
--- lib/models/log.rb
+++ lib/models/log.rb
@@ -1,21 +1,17 @@
 module Models
   class Log
-    attr_accessor :id, :created_at, :status, :data
+    attr_accessor :id, :created_at, :updated_at, :status, :data
 
     def initialize(attrs = {})
       attrs.each do |key, value|
         self.send("#{key}=", value) if respond_to?("#{key}=")
       end
-    end
+    end 
 
     def attributes_hash
       instance_variables.each_with_object({}) do |var, hash|
         hash[var.to_s.delete('@').to_sym] = instance_variable_get(var)
       end
-    end
-
-    def updated_at=(date)
-      @updated_at = date || DateTime.now
     end
 
     def data=(data)
